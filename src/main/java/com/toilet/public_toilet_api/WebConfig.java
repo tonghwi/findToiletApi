986f73f4d5cf251b14a4f1e0d5bae42642c83cdd
ehdgnl1215 <ehdgnl1215@naver.com>
2025-06-03 23:02:08 +0900

    CORS 설정 변경

--- src/main/java/com/toilet/public_toilet_api/WebConfig.java
+++ src/main/java/com/toilet/public_toilet_api/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // API 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트 주소
+                .allowedOrigins("http://localhost:5173") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*");
                 //.allowCredentials(true); // 쿠키 포함 허용 시
